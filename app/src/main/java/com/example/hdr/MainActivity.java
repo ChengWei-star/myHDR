8ca694659bd8dc0738f6fa78b1507c68f12a96d7
shyshyshy <45659362+ChengWei-star@users.noreply.github.com>
2023-08-31 20:21:31 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/hdr/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.hdr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 2000;
+
+    //Variablse
+    Animation topAnim;
+    ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Animation
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+
+        //Hooks
+        image = findViewById(R.id.imgLogo);
+
+        image.setAnimation(topAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
+    }
+}
